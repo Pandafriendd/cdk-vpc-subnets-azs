e8fe00902df506f1fad37f814f4c128f0c5f733a
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-01-05 23:25:59 +0000

    update source

--- bin/cdk-vpc-subnets-az.js
+++ bin/cdk-vpc-subnets-az.js
@@ -4,4 +4,7 @@ const cdk = require('@aws-cdk/core');
 const { CdkVpcSubnetsAzStack } = require('../lib/cdk-vpc-subnets-az-stack');
 
 const app = new cdk.App();
-new CdkVpcSubnetsAzStack(app, 'CdkVpcSubnetsAzStack');
+
+const _env = { account: '457175632986', region: 'ap-southeast-1' };
+
+new CdkVpcSubnetsAzStack(app, 'CdkVpcSubnetsAzStack', { env: _env });
